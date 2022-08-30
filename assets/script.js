2dbb64aa5470d47d27f79a46e61adf7803005cf6
Noah Brimhall <noahbrimhall6@gmail.com>
2022-08-29 19:53:26 -0600

    allow users to select a movie to add to thier playlist and saves playlist in localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,80 @@
-var movieSearch = document.querySelector("#movieSearch");
-var searchBtn = document.querySelector("#searchBtn");
-var searchResults = document.querySelector("#searchResults");
+var movieSearch = $("#movieSearch");
+var searchBtn = $("#searchBtn");
+var searchResults = $("#searchResults");
+var moviePlaylistEl = $("#movieplist");
 
 var omdbapi = "http://www.omdbapi.com/?apikey=ab9eb185&s=";
+var movies;
+var playlist = [];
 
 
-function onSubmit(event) {
+
+function renderSearch(results) {
+  searchResults.html("");
+  movies = results.Search;
+  
+  for (i = 0; i < movies.length; i++) {
+    var li = $("<li>");
+    li.text(movies[i].Title);
+    li.attr("data-index", i);
+    searchResults.append(li);
+
+    li.on("click", saveMovie);
+  }
+
+}
+
+function saveMovie(event) {
+  var selection = $(event.target).attr("data-index");
+  playlist.push(movies[selection]);
+  storePlaylist();
+  renderPlaylist();
+}
+
+function initPlaylist() {
+  var storedPlaylist = JSON.parse(localStorage.getItem("playlist"));
+  if (storedPlaylist !== null) {
+    playlist = storedPlaylist;
+  }
+
+  renderPlaylist();
+}
+
+function storePlaylist() {
+  localStorage.setItem("playlist", JSON.stringify(playlist));
+}
+
+function renderPlaylist() {
+  moviePlaylistEl.html("");
+
+  for (var i = 0; i < playlist.length; i++) {
+    var div = $("<div>");
+    div.text(playlist[i].Title);
+    div.attr("data-index", i);
+    var button = $("<button>");
+    button.text("delete");
+    moviePlaylistEl.append(div);
+    div.append(button);
+
+  }
+}
+
+moviePlaylistEl.on("click", function(event) {
+  var element = $(event.target);
+
+  if (element.is("button")) {
+    var index = element.parent().attr("data-index");
+    playlist.splice(index, 1);
+
+    storePlaylist();
+    renderPlaylist();
+  }
+});
+
+searchBtn.on("click", function(event) {
   event.preventDefault();
-
-  var userSearch = movieSearch.value.trim();
+  
+  var userSearch = movieSearch.val().trim();
   if (userSearch === "") {
     return;
   }
@@ -19,23 +85,10 @@ function onSubmit(event) {
   })
   .then(function (data) {
     renderSearch(data);
+    console.log(data);
   });
 
-  movieSearch.value = "";
-}
+  movieSearch.val("");
+});
 
-function renderSearch(results) {
-  searchResults.innerHTML = "";
-  var movies = results.Search;
-  
-  for (i = 0; i < movies.length; i++) {
-    var li = document.createElement("li");
-    li.textContent = movies[i].Title;
-    li.setAttribute("data-index", i);
-    searchResults.appendChild(li);
-  }
-
-
-}
-
-searchBtn.addEventListener("click", onSubmit);
+initPlaylist();
