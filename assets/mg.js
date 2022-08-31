f964aeb234cc680460380f9d0ff4a6c5686e8b68
ABoss <ashleyjeanbostrom@gmail.com>
2022-08-31 17:48:29 -0600

    update to js and added jquery link to html

--- assets/mg.js
+++ assets/mg.js
@@ -7,8 +7,28 @@ var moviePlaylistEl = $("movieplist");
 console.log('Genre Selection', genreSelEl.val());
 console.log('Mood Selection', moodSelEl.val());
 
-var tmdbapi = 'https://api.themoviedb.org/3/movie/76341?api_key=c560270447805eeaa48cfdda957f60b7';
+var tmdbapi = 'https://api.themoviedb.org/3/movie/76341?api_key=bd949d583d67c785ccc8d2de7703c463';
 var movies;
 var playlist = [];
 
+document.addEventListener('DOMContentLoaded', function () {
+    var input = document.this.getElementById('genreSel');
+    if (localStorage ['genreSel]']) { //if genre is chosen
+        input.value = localStorage['genreSel']; //set the value
+    }
+    input.onchange = function () {
+        localStorage['genreSel'] = this.value; //change localStorage on change
+    }
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    var input = document.this.getElementById('moodSel');
+    if (localStorage ['moodSel]']) { //if mood is chosen
+        input.value = localStorage['moodSel']; //set the value
+    }
+    input.onchange = function () {
+        localStorage['moodSel'] = this.value; //change localStorage on change
+    }
+});
+
+let response = fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=bd949d583d67c785ccc8d2de7703c463&language=en-US')
