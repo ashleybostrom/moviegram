6ff1482d7f2f025cf9656c37ffe10de779903876
ABoss <ashleyjeanbostrom@gmail.com>
2022-09-01 02:14:35 -0600

    added some to the storage and render as well as img

--- assets/mg.js
+++ assets/mg.js
@@ -4,22 +4,13 @@ var moodSelEl = $("moodSel");
 var moodResults = $("moodResults-btn");
 var moviePlaylistEl = $("movieplist");
 
-console.log('Genre Selection', genreSelEl.val());
 console.log('Mood Selection', moodSelEl.val());
+console.log('Genre Selection', genreSelEl.val());
 
 var tmdbapi = 'https://api.themoviedb.org/3/movie/76341?api_key=bd949d583d67c785ccc8d2de7703c463';
+var imgpath = 'https://image.tmdb.org/t/p/w1280';
 var movies;
 var playlist = [];
-
-document.addEventListener('DOMContentLoaded', function () {
-    var input = document.getElementById('genres');
-    if (localStorage ['genreSel']) { //if genre is chosen
-        input.value = localStorage['genreSel']; //set the value
-    }
-    input.onchange = function () {
-        localStorage['genreSel'] = this.value; //change localStorage on change
-    }
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     var input = document.getElementById('moods');
@@ -31,12 +22,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+let genreresaction = fetch('https://api.themoviedb.org/3/discover/movie?api_key=bd949d583d67c785ccc8d2de7703c463&language=en-US&include_adult=false&include_video=false&page=1&with_genres=true&total_results=10')
+    //.then(res => res.json()).then(data => console.log(data));
+      .then(res => res.json()).then(function(data)
 
-// let response = fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=bd949d583d67c785ccc8d2de7703c463&language=en-US');
-
-let genreresaction = fetch('https://api.themoviedb.org/3/discover/movie?api_key=bd949d583d67c785ccc8d2de7703c463&language=en-US&include_adult=false&include_video=false&page=1&with_genres=true&total_results=10');
-.then(res => res.json()).then(data => console.log(data)); 
-console.log(genreresaction);
-console.log(genreresaction.page);
-    
-
+    document.addEventListener('DOMContentLoaded', function () {
+        var input = document.getElementById('genres');
+        if (localStorage ['genreSel']) { //if genre is chosen
+            input.value = localStorage['genreSel']; //set the value
+        }
+        input.onchange = function () {
+            localStorage['genreSel'] = this.value; //change localStorage on change
+        }
+    });
